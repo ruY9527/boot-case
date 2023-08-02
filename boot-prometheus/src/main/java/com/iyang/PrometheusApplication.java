0bdb3cc6372708a94dd48c8c103b187dc9dcac0c
ruY9527 <1411091515@qq.com>
2023-08-02 20:12:42 +0800

    feat: 监控测试

---
+++ boot-prometheus/src/main/java/com/iyang/PrometheusApplication.java
@@ -0,0 +1,22 @@
+package com.iyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/****
+ * author: BaoYang
+ * date: 2023/8/2
+ * desc:
+ ***/
+
+@SpringBootApplication
+public class PrometheusApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PrometheusApplication.class, args);
+
+
+    }
+
+}
