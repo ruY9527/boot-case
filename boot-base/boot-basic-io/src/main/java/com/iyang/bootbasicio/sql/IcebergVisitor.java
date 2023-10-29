5adb06f8a98c094641877892e1c0620de5b7bff4
ruY9527 <1411091515@qq.com>
2023-10-30 07:56:42 +0800

    feat: g4测试编写

---
+++ boot-base/boot-basic-io/src/main/java/com/iyang/bootbasicio/sql/IcebergVisitor.java
@@ -0,0 +1,72 @@
+package com.iyang.bootbasicio.sql;
+
+import lombok.extern.slf4j.Slf4j;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/***
+ * @author: yang_bao
+ * @date: 2023/10/30
+ * @desc:
+ ***/
+
+@Slf4j
+public class IcebergVisitor extends SqlBaseBaseVisitor {
+
+
+    private List<String> tableList = new ArrayList<>();
+
+    @Override
+    public List<String> visitSingleStatement(SqlBaseParser.SingleStatementContext ctx) {
+        super.visitSingleStatement(ctx);
+
+
+        return tableList;
+    }
+
+
+    @Override
+    public List<String> visitTableIdentifier(SqlBaseParser.TableIdentifierContext ctx) {
+        String db = ctx.db.getText();
+        String table = ctx.table.getText();
+
+        System.out.println("1111");
+        log.info("the visitTableIdentifier db value is --> {} , table is ---> {} " , db, table);
+        return tableList;
+    }
+
+
+    @Override
+    public Object visitInsertOverwriteTable(SqlBaseParser.InsertOverwriteTableContext ctx) {
+
+        return super.visitInsertOverwriteTable(ctx);
+    }
+
+/*    @Override
+    public Object visitSingleInsertQuery(SqlBaseParser.SingleInsertQueryContext ctx) {
+        String queryTermText = ctx.queryTerm().getText();
+        String queryStart = ctx.queryTerm().getStart().getText();
+        String queryStop = ctx.queryTerm().getStop().getText();
+
+
+        SqlBaseParser.QueryTermContext termContext = ctx.queryTerm();
+
+        log.info("the visitSingleInsertQuery.queryTerm() --> {}, start ---> {} , stop ---> {} ", queryTermText, queryStart, queryStop);
+        return super.visitSingleInsertQuery(ctx);
+    }*/
+
+    @Override
+    public Object visitUpdateTable(SqlBaseParser.UpdateTableContext ctx) {
+
+
+        return super.visitUpdateTable(ctx);
+    }
+
+
+    @Override
+    public Object visitDeleteFromTable(SqlBaseParser.DeleteFromTableContext ctx) {
+        return super.visitDeleteFromTable(ctx);
+    }
+}
---
+++ boot-base/boot-basic-io/src/main/java/com/iyang/bootbasicio/sql/SqlFormatMain.java
@@ -0,0 +1,35 @@
+package com.iyang.bootbasicio.sql;
+
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.atn.PredictionMode;
+import org.apache.spark.sql.catalyst.parser.UpperCaseCharStream;
+
+/***
+ * @author: yang_bao
+ * @date: 2023/10/30
+ * @desc:
+ ***/
+public class SqlFormatMain {
+
+    public static void main(String[] args) {
+
+        String sql = "insert overwrite table a.table_test123\n" +
+                "select * from bao_test.table_test";
+
+        // String sql = "select * from bao_test.table_test";
+
+        UpperCaseCharStream charStream = new UpperCaseCharStream(CharStreams.fromString(sql));
+        SqlBaseLexer lexer = new SqlBaseLexer(charStream);
+        CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
+        SqlBaseParser parser = new SqlBaseParser(commonTokenStream);
+        parser.getInterpreter().setPredictionMode(PredictionMode.SLL);
+
+        IcebergVisitor sparkSqlAst = new IcebergVisitor();
+        Object visit = sparkSqlAst.visit(parser.singleStatement());
+
+
+
+    }
+
+}
